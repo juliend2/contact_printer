c08a4c7ad7f6e4867c559f784f4202cdb52c3cdd
Julien Desrosiers <juliend2@gmail.com>
2012-01-12 17:38:33 -0500

    created a Contact class. put all classes in a ContactPrinter module

--- contact_printer.rb
+++ contact_printer.rb
@@ -1,17 +1,36 @@
 require 'rubygems'
 require 'pdfkit'
 
-class ContactPrinter
-  def initialize(path_to_contacts)
-    @path_to_contacts = path_to_contacts.chomp("/")
+module ContactPrinter
+
+  class Contact
+    def initialize(path_to_contact)
+      @path_to_contact = path_to_contact
+    end
+
+    def to_html
+    end
+
+    def text
+      return @text unless @text.nil?
+      @text = File.open(@path_to_contact).read
+    end
   end
 
-  def contacts
-    @contacts ||= Dir.glob("#{@path_to_contacts}/*.md")
+  class Printer
+    def initialize(path_to_contacts)
+      @path_to_contacts = path_to_contacts.chomp("/")
+      @contact_files = Dir.glob("#{@path_to_contacts}/*.md")
+    end
+
+    def contacts
+      return @contacts unless @contacts.nil?
+      @contacts = @contact_files.map{|file| Contact.new file }
+    end
   end
 
 end
 
-contact_printer = ContactPrinter.new '/Users/jdesrosiers/Dropbox/perso/contacts'
+contact_printer = ContactPrinter::Printer.new '/Users/jdesrosiers/Dropbox/perso/contacts'
 puts contact_printer.contacts
 
