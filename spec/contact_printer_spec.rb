129df9c9e0d1f19a0de7cb01cb18fecfc7422817
Julien Desrosiers <juliend2@gmail.com>
2012-01-12 22:11:34 -0500

    added some specs

--- spec/contact_printer_spec.rb
+++ spec/contact_printer_spec.rb
@@ -7,6 +7,8 @@ describe ContactPrinter::Contact do
     before do
       @antony = ContactPrinter::Contact.new './fixtures/antony-chevrolet.md'
       @julius = ContactPrinter::Contact.new './fixtures/julius-caesar.md'
+      @john = ContactPrinter::Contact.new './fixtures/john-doe.md'
+      @jean = ContactPrinter::Contact.new './fixtures/jean-qwerty.md'
     end
     it "should have a name" do
       @antony.name.should == 'Antony Chevrolet'
@@ -14,8 +16,16 @@ describe ContactPrinter::Contact do
     it "should have two phone numbers" do
       @antony.phones.should == "<ul><li>514-555-1673</li><li>514-555-1234</li></ul>"
     end
+    it "should have no phone number" do
+      @john.phones.should be_nil
+    end
+    it "should have one email and one phone number" do
+      @jean.emails.should == "<ul><li>jeanqwerty@telecom.ca</li></ul>"
+      @jean.phones.should == "<ul><li>514-555-9652</li></ul>"
+    end
     it "should have two emails" do
       @julius.emails.should == "<ul><li>julius@gmail.com</li><li>julius@hotmail.com</li></ul>"
+      @john.emails.should == "<ul><li>has.no@phonenumbers.com</li><li>but.has@emails.com</li></ul>"
     end
     it "should render to HTML" do
       @julius.to_html.should == %{<h2>Julius Caesar</h2>
