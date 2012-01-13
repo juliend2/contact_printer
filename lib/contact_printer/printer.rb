ac00e8a7306f2fd18089df26e0b535653fa32fae
Julien Desrosiers <juliend2@gmail.com>
2012-01-13 09:10:27 -0500

    load config file

--- lib/contact_printer/printer.rb
+++ lib/contact_printer/printer.rb
@@ -1,22 +1,32 @@
-require 'rubygems'
-# require 'pdfkit'
+require 'yaml'
 
 module ContactPrinter
 
   class Printer
-    def initialize(path_to_contacts)
-      @path_to_contacts = path_to_contacts.chomp("/")
+    def initialize(path_to_contacts, path_to_config_file)
+      @path_to_contacts = path_to_contacts.chomp "/"
+      @path_to_config = path_to_config_file
+      @config = YAML::load File.open(@path_to_config)
       @contact_files = Dir.glob("#{@path_to_contacts}/*.md")
     end
 
     def contacts
       return @contacts unless @contacts.nil?
-      @contacts = @contact_files.map{|file| Contact.new file }
+      @contacts = @contact_files.select {|file| 
+        selected_contacts.include? File.basename(file).chomp('.md')
+      }.map {|file|
+        Contact.new file
+      }
+    end
+
+    def selected_contacts
+      @config['contacts']
+    end
+
+    def render
+      kit = PDFKit.new(html, :page_size => 'Letter')
     end
   end
 
 end
-
-# contact_printer = ContactPrinter::Printer.new '/Users/jdesrosiers/Dropbox/perso/contacts'
-# puts contact_printer.contacts[3].to_html
 
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe ContactPrinter::Printer do
   context "a Printer object" do
     before do
-      @printer = ContactPrinter::Printer.new "spec/fixtures"
+      @printer = ContactPrinter::Printer.new "spec/fixtures", "spec/config.yml"
     end
-    it "should have 4 contacts" do
-      @printer.contacts.size.should == 4
+    it "should have 3 contacts" do
+      @printer.contacts.size.should == 3
     end
   end
 end
